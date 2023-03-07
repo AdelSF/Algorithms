806c02854f74f817c69c3da14519e49c18027fa5
Paakraad <paakraad@Adels-MBP-2.attlocal.net>
2023-03-06 23:35:37 -0800

    adding new algo

--- Arrays/nonConstructibleChange.js
+++ Arrays/nonConstructibleChange.js
@@ -1,4 +1,19 @@
-// minimum amount of coins
+// minimum amount of change that we can not create, we can use a coin only once.
 
 
+
+function nonConstructibleChange(coins) {
+    // Write your code here.
+  coins.sort((a, b) => a - b);
+  
+    let currentChangeCreated = 0;
+    
+    for (const coin of coins) {
+      if (coin > currentChangeCreated + 1) {
+        return currentChangeCreated + 1
+      }
+      currentChangeCreated += coin;
+    }
+    return currentChangeCreated + 1
+  }
 
